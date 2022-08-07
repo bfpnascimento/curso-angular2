537a1e032dfa543080fa10872d8346c0d519d129
Bruno Nascimento <bfpnascimento@gmail.com>
2022-08-08 00:02:28 +0100

    aula 13

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
 import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    InputPropertyComponent
   ],
   imports: [
     BrowserModule,
